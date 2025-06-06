4e16de3ca33ed8deba398c925d364898b5dde0e2
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-06-06 01:11:16 -0400

    React_router_ install

--- Google/src/component/Navbar.jsx
+++ Google/src/component/Navbar.jsx
@@ -1,8 +1,19 @@
-function Navbar(){
-    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-centers border-b border-gray-200">
-          <div className="flex justify-between items-center space-x-5 w-screen">
+import { Link } from 'react-router-dom';
 
-          </div>
-    </div>
+function Navbar(){
+   
+          
+        return( 
+            <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-centers border-b border-gray-200">
+                   <div className="flex justify-between items-center space-x-5 w-screen">
+                         <Link to="/">
+                                <p className='text-2xl'>
+                                    HaiSearch<i className="ri-search-line"></i>
+                                </p>
+                         </Link>
+                  </div>
+            </div>
+        )  
+
 }
 export default Navbar
