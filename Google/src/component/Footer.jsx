37175f50a017f95be37cfce42da12f675d66af61
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-06-09 19:18:34 -0400

    01001

--- Google/src/component/Footer.jsx
+++ Google/src/component/Footer.jsx
@@ -1,4 +1,9 @@
 function Footer(){
-    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200"></div>
+  const year = Date.getYear() ;
+    return(
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+          {year}
+    </div>
+    )
 }
 export default Footer
--- Google/src/component/Routes.jsx
+++ Google/src/component/Routes.jsx
@@ -1,18 +1,18 @@
-import { Switch , Route , Redirect } from 'react-router-dom' ;
+import { Routes as Switch, Route, Navigate } from 'react-router-dom';
 import Result from './Result';
-function Routes(){
-  
-    return(
-        <div className=" p-4 ">
-              <Switch>
-                       <Route exact path=' / '>
-                               <Redirect to='/search' />
-                       </Route>
-                       <Route exact path={['/search' , '/images' , '/news' , '/videos']}>
-                               <Result/>
-                       </Route>
-              </Switch>
-        </div>
-    )
+
+function Routes() {
+  return (
+    <div className="p-4">
+      <Switch>
+        <Route path="/" element={<Navigate to="/search" />} />
+        <Route path="/search" element={<Result />} />
+        <Route path="/images" element={<Result />} />
+        <Route path="/news" element={<Result />} />
+        <Route path="/videos" element={<Result />} />
+      </Switch>
+    </div>
+  );
 }
-export default Routes
+
+export default Routes;
