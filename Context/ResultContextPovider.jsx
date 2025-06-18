84d9d4e7a74537e18828eaa416a1a15d85154401
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-06-17 23:49:09 -0400

    00010

--- Context/ResultContextPovider.jsx
+++ Context/ResultContextPovider.jsx
@@ -1,21 +1,35 @@
 import React , { createContext, useContext , useState} from 'react'
 
-const ResultContext = createcontext();
-const baseUrl = 'https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures'
-/* 'AIzaSyBZSZuOlBtgSB_QUcLhW8vWgOGWglVgdL0' */
+const ResultContext = createContext();
+const baseUrl = 'https://google-search-api-fast.p.rapidapi.com'
+/* '2c35bfe629b34237fa6142f19610f1faa101f50e968436ea08b0317ee7511b24' */
 
 export const ResultContextProvider = ({children}) => {
        const[results,setResults] = useState([]);
        const [isLoading,setIsLoading] = useState(false);
        const [searchTerm ,setSearchTerm] = useState("");
+       
 
        const getResults = async (type) => {
            setIsLoading(true);
 
            const response = await fetch(`${baseUrl}${type}` ,{
-            
-           }
-       }
+              method:'GET',
+              headers: {
+		              'x-rapidapi-key': '47a2ac3bcdmsh4fb0107a908323ep13c651jsn9301b5cf4956',
+		              'x-rapidapi-host': 'google-search-api-fast.p.rapidapi.com'
+	          }
+           });
 
+           const data = await response.json() ;
+
+           setResults(data);
+           setIsLoading(false);
+       }
+       return (
+             <ResultContext.Provider value={{ getResults, results , searchTerm , setSearchTerm , isLoading}}>
+
+                </ResultContext.Provider>
+       )
 
 }
